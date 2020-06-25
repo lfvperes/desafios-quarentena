04d7be312c05474304f33e24e8dbf011fc0f7cbe
Luís Peres <luisfvperes@gmail.com>
2020-06-25 06:45:16 -0300

    Desafio + bonus 1
    Comando `/help` funcionando. Token em arquivo separado.

---
+++ Desafio 6/javascript/commands.js
@@ -0,0 +1,5 @@
+const commandsList = {
+    jokempo: 'Plays Jan Ken Po'
+}
+
+module.exports = {commandsList}
--- Desafio 6/javascript/index.js
+++ Desafio 6/javascript/index.js
@@ -2,17 +2,36 @@ process.env.NTBA_FIX_319 = true; // Silences an annoying error message.
 const TelegramBot = require('node-telegram-bot-api');
 const jokempo = require('./jokempo');
 const randomPhrases = require('./random-phrases');
+const tokenFile = require('./token');
+const commands = require('./commands');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR ACCESS TOKEN HERE';
+const token = tokenFile.token;
 
 if (token === 'YOUR ACCESS TOKEN HERE') {
-	console.log('You forgot to replate the access token!!! Properly read the README before continuing >:(');
+	console.log('You forgot to replace the access token!!! Properly read the README before continuing >:(');
 	process.exit(-1);
 }
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
+
+// DESAFIO 
+bot.onText(/\/help/, async (msg, match) => {
+	const chatId = msg.chat.id;
+
+	//console.log(msg);
+	//console.log(match);
+	let resp = '';
+	const commandsList = commands.commandsList;
+	for (const commandName in commandsList) {
+		resp += '/' + commandName + ' - ' + commandsList[commandName] + '\n';
+	}
+	
+	console.log(resp);
+	bot.sendMessage(chatId, resp);
+	//bot.sendMessage(chatId, match[1]);
+})
 
 // Listen for any kind of message. There are different kinds of
 // messages.
@@ -32,4 +51,5 @@ console.log('Fetching data...');
 bot.getMe().then(me => {
 	console.log(`I'm ready to serve! Talk to me on @${me.username}`);
 	console.log(`or visit this link: https://t.me/${me.username}`);
+	//console.log(bot.getMyCommands());
 });
